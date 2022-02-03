f521f8c229c4505a4fd29cced7b5aecaa3a45716
MendietaSergio <mendieta.sergio.1995@gmail.com>
2022-02-03 20:51:37 -0300

    añade reducer cambio de estado 'terminado', falta completar

--- src/reducers/tareasReducer.js
+++ src/reducers/tareasReducer.js
@@ -1,11 +1,12 @@
 export const tareasReducer = (state = [], action) => {
   switch (action?.type) {
     case "agregar":
-        //AGREGAR ELEMENTOS AL ARRAY
+      //AGREGAR ELEMENTOS AL ARRAY
       return [...state, action.payload]; //+ elemento agregado
     case "borrar":
-        
-      return state.filter(tarea => tarea.id!== action.payload); //- elemento eliminado
+      return state.filter(tarea => tarea.id !== action.payload); //- elemento eliminado
+    case "estado":
+      return [...state, ]
     default:
       return state;
   }
