bf740650d01255204b0c6221deb4aecc9ccaad0d
MendietaSergio <mendieta.sergio.1995@gmail.com>
2022-02-04 19:09:48 -0300

    añade componente formulario

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,15 +4,18 @@ import { Button } from './Button';
 export const Form = ({ handleSubmit, handleInputChange, descripcion }) => {
     return <div>
         <form onSubmit={handleSubmit}>
-            <input
-                type="text"
-                name="descripcion"
-                pladeholder="Aprender qué...."
-                autoComplete="off"
-                className="from-control"
-                onChange={handleInputChange}
-                value={descripcion}
-            />
+            <div class="form-group">
+                <input
+                    id="label-tarea"
+                    type="text"
+                    name="descripcion"
+                    placeholder="Aprender qué...."
+                    autoComplete="off"
+                    className="form-control"
+                    onChange={handleInputChange}
+                    value={descripcion}
+                />
+            </div>
             <Button
                 className=" btn btn-primary w-100 mt-2"
                 text="Agregar"
