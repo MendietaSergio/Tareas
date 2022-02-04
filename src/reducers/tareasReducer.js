e98be29ae6b8a7fdb3e4e5df89eef13716b319b6
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2022-02-04 00:13:00 -0300

    Añade cambio de estado a 'terminado'

--- src/reducers/tareasReducer.js
+++ src/reducers/tareasReducer.js
@@ -4,9 +4,13 @@ export const tareasReducer = (state = [], action) => {
       //AGREGAR ELEMENTOS AL ARRAY
       return [...state, action.payload]; //+ elemento agregado
     case "borrar":
-      return state.filter(tarea => tarea.id !== action.payload); //- elemento eliminado
+      return state.filter((tarea) => tarea.id !== action.payload); //- elemento eliminado
     case "estado":
-      return [...state, ]
+      return state.map((tarea) =>
+        tarea.id === action.payload
+          ? { ...tarea, terminado: !tarea.terminado }
+          : tarea
+      );
     default:
       return state;
   }
