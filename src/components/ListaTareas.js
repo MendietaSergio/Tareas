487c60dac78cc06bee2d2de9b896ac16e39eacff
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2022-02-04 00:12:17 -0300

    Añade clase tachado si 'terminado' es true

--- src/components/ListaTareas.js
+++ src/components/ListaTareas.js
@@ -1,24 +1,28 @@
-import React from 'react';
+import React from "react";
 
-export const ListaTareas = ({tareas,handleDelete}) => {
-  return <div>
-       {tareas.map(({ descripcion, terminado, id }, i) => (
-              <li
-                key={descripcion + i}
-                className="d-flex justify-content-between align-items-center"
-              >
-                <p>
-                  {i + 1}.<span className={terminado&&'text-decoration-line-through'}>
-                    {descripcion}
-                    </span> 
-                </p>
-                <button
-                  onClick={() => handleDelete(id)}
-                  className="btn btn-sm btn-danger mb-1"
-                >
-                  <i className="fas fa-trash-alt"></i>
-                </button>
-              </li>
-            ))}
-  </div>;
+export const ListaTareas = ({ tareas, handleDelete, handleComplete }) => {
+  return (
+    <div>
+      {tareas.map(({ descripcion, terminado, id }, i) => (
+        
+        <li
+        key={descripcion + i}
+        className="d-flex justify-content-between align-items-center"
+        >
+          <p onClick={() => handleComplete(id)}>
+            {i + 1}. 
+            <span className={terminado ? "text-decoration-line-through" : ""}>
+              {" "+descripcion}
+            </span>
+          </p>
+          <button
+            onClick={() => handleDelete(id)}
+            className="btn btn-sm btn-danger mb-1"
+          >
+            <i className="fas fa-trash-alt"></i>
+          </button>
+        </li>
+      ))}
+    </div>
+  );
 };
