d778b8e4a33a76f4485d2fbb0b1b52a5033e7003
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2022-02-06 04:27:37 -0300

    Añade mock , con datos de los resaltadores

---
+++ src/mock/highlighters.js
@@ -0,0 +1,32 @@
+export const highLighters = [
+  {
+    id: 1,
+    img: "/static/media/Resaltador_01.556d9f79b9cb44b0e388.png",
+    color: "color1",
+  },
+  {
+    id: 2,
+    img: "/static/media/Resaltador_02.66bdc701a87ab277f08e.png",
+    color: "color2",
+  },
+  {
+    id: 3,
+    img: "/static/media/Resaltador_03.c0aedd35743c9b85b7ab.png",
+    color: "color3",
+  },
+  {
+    id: 4,
+    img: "/static/media/Resaltador_04.9b020a1091b50c83e391.png",
+    color: "color4",
+  },
+  {
+    id: 5,
+    img: "/static/media/Resaltador_05.d70390cc7c0c6080f825.png",
+    color: "color5",
+  },
+  {
+    id: 6,
+    img: "/static/media/Resaltador_06.4b9c67f4d2d7afd5fbe9.png",
+    color: "color6",
+  }
+];
