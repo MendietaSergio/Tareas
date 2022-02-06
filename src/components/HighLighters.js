e7a6c63a991ffbfd7f450cb98e7e204a89e6e6a3
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2022-02-06 04:34:34 -0300

    Añade huglighters, icono de paleta colores, lista de resaltadores

---
+++ src/components/HighLighters.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import { highLighters } from "../mock/highlighters";
+import iconHighLighters from "../img/icon_highlighters.png";
+export const HighLighters = ({setChangeColor, changeColor}) => {
+  const [showHighLighters, setShowHighLighters] = useState(false);
+
+  const handleSelect =(color) =>{
+    setChangeColor(color)
+    setShowHighLighters(!showHighLighters)
+  }
+  return (
+    <>
+      <div className="col-6 d-flex justify-content-end mb-5">
+        <img
+          src={iconHighLighters}
+          title="Paleta de colores"
+          className="iconhighlighters"
+          onClick={() => setShowHighLighters(!showHighLighters)}
+        />
+      </div>
+      {showHighLighters ? (
+        <>
+          <div className="row">
+            <div className={showHighLighters&&`animate__animated animate__fadeInRight container_colores d-flex justify-content-end`}>
+              {highLighters.map((highLighter) => (
+                <div key={highLighter.id} className={`background${highLighter.id}`}>
+                  <img
+                    src={highLighter.img}
+                    title={highLighter.img}
+                    className="logos_highLighter"
+                    onClick={() => handleSelect(highLighter.color)}
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+        </>
+      ) : null}
+    </>
+  );
+};
