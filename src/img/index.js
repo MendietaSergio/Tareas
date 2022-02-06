e5220fb5d5c3b834053acb45114c150b26b02c86
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2022-02-06 04:27:57 -0300

    Añade cambios de posicion de la carpeta img

---
+++ src/img/index.js
@@ -0,0 +1,8 @@
+const resaltador_01 = require('./Resaltador_01.png')
+const resaltador_02 = require('./Resaltador_02.png')
+const resaltador_03 = require('./Resaltador_03.png')
+const resaltador_04 = require('./Resaltador_04.png')
+const resaltador_05 = require('./Resaltador_05.png')
+const resaltador_06 = require('./Resaltador_06.png')
+
+export {resaltador_01, resaltador_02, resaltador_03, resaltador_04, resaltador_05, resaltador_06}
