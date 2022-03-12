02fbe6066379f2d2eee6292123babcb23c148b3c
MendietaSergio <mendieta.sergio.1995@gmail.com>
2022-03-12 01:31:24 -0300

    añade alt a img

--- src/components/HighLighters.js
+++ src/components/HighLighters.js
@@ -2,10 +2,10 @@ import React, { useState } from "react";
 import { highLighters } from "../mock/highlighters";
 import iconHighLighters from "../img/icon_highlighters.png";
 
-export const HighLighters = ({setChangeColor, changeColor}) => {
+export const HighLighters = ({ setChangeColor, changeColor }) => {
   const [showHighLighters, setShowHighLighters] = useState(false);
 
-  const handleSelect =(color) =>{
+  const handleSelect = (color) => {
     setChangeColor(color)
     setShowHighLighters(!showHighLighters)
   }
@@ -23,12 +23,13 @@ export const HighLighters = ({setChangeColor, changeColor}) => {
       {showHighLighters ? (
         <>
           <div className="row">
-            <div className={showHighLighters&&`animate__animated animate__fadeInRight container_colores d-flex justify-content-end`}>
+            <div className={showHighLighters && `animate__animated animate__fadeInRight container_colores d-flex justify-content-end`}>
               {highLighters.map((highLighter) => (
                 <div key={highLighter.id} className={`background${highLighter.id}`}>
                   <img
                     src={highLighter.img}
-                    title={"Resaltador"}
+                    title="Resaltador"
+                    alt="Resaltador"
                     className="logos_highLighter"
                     onClick={() => handleSelect(highLighter.color)}
                   />
