eb0b758966ffed42443290483a220ba03f7a8aa5
MendietaSergio <mendieta.sergio.1995@gmail.com>
2022-03-11 21:47:41 -0300

    Añade url de img resaltadores

--- src/mock/highlighters.js
+++ src/mock/highlighters.js
@@ -1,32 +1,32 @@
 export const highLighters = [
   {
     id: 1,
-    img: "/static/media/Resaltador_01.556d9f79b9cb44b0e388.png",
+    img: "https://i.imgur.com/OtrKMpb.png",
     color: "color1",
   },
   {
     id: 2,
-    img: "/static/media/Resaltador_02.66bdc701a87ab277f08e.png",
+    img: "https://i.imgur.com/N0teHRi.png",
     color: "color2",
   },
   {
     id: 3,
-    img: "/static/media/Resaltador_03.c0aedd35743c9b85b7ab.png",
+    img: "https://i.imgur.com/IC0tY8O.png",
     color: "color3",
   },
   {
     id: 4,
-    img: "/static/media/Resaltador_04.9b020a1091b50c83e391.png",
+    img: "https://i.imgur.com/Ga2i0ri.png",
     color: "color4",
   },
   {
     id: 5,
-    img: "/static/media/Resaltador_05.d70390cc7c0c6080f825.png",
+    img: "https://i.imgur.com/bm3t68Q.png",
     color: "color5",
   },
   {
     id: 6,
-    img: "/static/media/Resaltador_06.4b9c67f4d2d7afd5fbe9.png",
+    img: "https://i.imgur.com/Pmd3Fci.png",
     color: "color6",
   }
 ];
